0de84cb81efe2acc6d6511f9dc235388325f9709
Gilbert <steven.n.gilbert@gmail.com>
2017-04-19 10:20:13 -0400

    v1.1

--- index.js
+++ index.js
@@ -6,14 +6,8 @@ function growPanel(pan) {
 
     if (classes.indexOf('open') === -1) {
       pan.classList.add('open');
-
-      // Tranforms p:nth-child(odd) to show text
-      document.styleSheets[0].addRule('.open p:nth-child(odd)', 'transform: translateY(0px)');
     } else {
       pan.classList.remove('open');
-
-      // Tranforms p:nth-child(odd) to hide text
-      document.styleSheets[0].deleteRule(16);
     }
   };
 }
